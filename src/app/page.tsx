3fb4f44642eb6c005638ed931d46698f578ca912
Leonel Rodriguez <leoneljrodriguez@gmail.com>
2023-08-14 08:41:26 -0400

    🚀 first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Container from "@/components/Container";
+
+/**
+ * Home component represents the main page of the application.
+ *
+ * @module Home
+ * @returns {JSX.Element} The Home component.
+ */
+const Home = () => {
+  return <Container className="">HOME</Container>;
+};
+
+export default Home;
